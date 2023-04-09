8583bea35460634773838b430aec51ac69392ec3
Oscar Santos <oscarsantosmu@gmail.com>
2023-04-08 23:13:50 -0600

    fix: add rnd text to app name

--- main.tf
+++ main.tf
@@ -52,7 +52,7 @@ resource "azurerm_service_plan" "app_plan" {
 }
 
 resource "azurerm_linux_function_app" "fcnapp" {
-  name                = "existingfcnapp"
+  name                = "fcnapp${random_id.random_id.hex}"
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
 
