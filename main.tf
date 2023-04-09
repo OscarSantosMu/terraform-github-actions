adc79075c463f07c9d4e7537d7237c10d20a85c2
Oscar Santos <oscarsantosmu@gmail.com>
2023-04-08 19:51:11 -0600

    feat: add fcn app

--- main.tf
+++ main.tf
@@ -27,19 +27,30 @@ resource "azurerm_resource_group" "rg" {
   location = local.location
 }
 
-resource "azurerm_service_plan" "linux_app_plan" {
-  name                = "appserviceplan"
+resource "azurerm_storage_account" "storageacc" {
+  name                     = "existingfcnapp"
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "azurerm_service_plan" "app_plan" {
+  name                = "example-app-service-plan"
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
   os_type             = "Linux"
-  sku_name            = "P1v2"
+  sku_name            = "Y1"
 }
 
-resource "azurerm_linux_web_app" "linux_web_app" {
-  name                = "existing-app"
+resource "azurerm_linux_function_app" "fcnapp" {
+  name                = "example-linux-function-app"
   resource_group_name = azurerm_resource_group.rg.name
-  location            = azurerm_service_plan.linux_app_plan.location
-  service_plan_id     = azurerm_service_plan.linux_app_plan.id
+  location            = azurerm_resource_group.rg.location
+
+  storage_account_name       = azurerm_storage_account.storageacc.name
+  storage_account_access_key = azurerm_storage_account.storageacc.primary_access_key
+  service_plan_id            = azurerm_service_plan.app_plan.id
 
   site_config {}
 }
