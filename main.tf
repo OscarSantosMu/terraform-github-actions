64d829b431a9d53e7d812d14b1d5b4608d1be4d9
Oscar Santos <oscarsantosmu@gmail.com>
2023-04-08 19:26:01 -0600

    feat: add app service to prod

--- main.tf
+++ main.tf
@@ -22,7 +22,24 @@ provider "azurerm" {
 }
 
 # Define any Azure resources to be created here. A simple resource group is shown here as a minimal example.
-resource "azurerm_resource_group" "rg-test" {
+resource "azurerm_resource_group" "rg" {
   name     = local.resource_group_name
   location = local.location
+}
+
+resource "azurerm_service_plan" "linux_app_plan" {
+  name                = "appserviceplan"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  os_type             = "Linux"
+  sku_name            = "P1v2"
+}
+
+resource "azurerm_linux_web_app" "linux_web_app" {
+  name                = "existing-app"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_service_plan.linux_app_plan.location
+  service_plan_id     = azurerm_service_plan.linux_app_plan.id
+
+  site_config {}
 }
